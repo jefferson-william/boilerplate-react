5d215d8bb040dc3791288e51fc7f88ae9f54941a
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-07-13 22:22:00 -0300

    feat: material-ui applied

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
+import { CssBaseline } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/styles'
 import history from '~/lib/history'
 import { Routers } from '~/routers'
 import { GlobalStyles } from '~/styles/global'
+import { defaultTheme } from '~/styles/themes/default'
 
 const Application: React.FC = () => {
   return (
-    <>
-      <Routers history={history} />
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-    </>
+      <CssBaseline />
+      <Routers history={history} />
+    </ThemeProvider>
   )
 }
 
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import logo from '~/logo.svg'
-import './index.css'
+import { Header } from '~/components/Header'
 
 export const HomePage: React.FC = () => {
   return (
     <div className="Home">
-      <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/index.tsx</code> and save to reload.
-        </p>
-        <a className="Home-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   )
 }
