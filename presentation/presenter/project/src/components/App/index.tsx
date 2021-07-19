e4c1d5fa6af93b6b3b223ef0a14809b289bb45a8
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-07-19 19:38:17 -0300

    chore: created vendor and center files in ui/react

--- presentation/presenter/project/src/components/App/index.tsx
+++ presentation/presenter/project/src/components/App/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { hot } from 'react-hot-loader/root'
 import { CssBaseline } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/styles'
-import history from '~/lib/history'
+import history from '@app/react/src/lib/history'
+import { GlobalStyles } from '@app/react/src/styles/global'
+import { defaultTheme } from '@app/react/src/styles/themes/default'
+import '@app/react/src/i18n'
 import { Routers } from '~/routers'
-import { GlobalStyles } from '~/styles/global'
-import { defaultTheme } from '~/styles/themes/default'
 import { DefaultSuspense } from '~/components/DefaultSuspense'
-import '~/i18n'
 
 const Application: React.FC = () => {
   return (
--- presentation/ui/react/src/setupTests.ts
+++ presentation/ui/react/src/setupTests.ts
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/react/dont-cleanup-after-each'
 import 'jest-styled-components'
 import 'mutationobserver-shim'
-import { server } from '~/mocks/server'
+import { server } from '@app/react/src/mocks/server'
 
 global.MutationObserver = global.window.MutationObserver
 
