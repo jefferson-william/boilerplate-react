73edfc91ca95ca9fdddc3d7ffd075a1d0c947a89
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-08-02 10:51:29 -0300

    fix: fixed envs

--- presentation/ui/react/src/react-app-env.d.ts
+++ presentation/ui/react/src/react-app-env.d.ts
@@ -1,1 +1,5 @@
+/// <reference types="react" />
+/// <reference types="react-dom" />
+/// <reference types="react-router-dom" />
 /// <reference types="react-scripts" />
+/// <reference types="@types/jest" />
