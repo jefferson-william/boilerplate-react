15b6ca4a148f24a7c551b585eed5caeff22f388c
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-09-21 17:12:23 -0300

    refactor(builder/craco): removed by unused

--- presentation/builder/craco/entries.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'increment-decrement': './src/components/IncrementDecrement/index.tsx',
-}
--- presentation/builder/craco/src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-/// <reference types="react-dom" />
-/// <reference types="react-router-dom" />
-/// <reference types="react-scripts" />
-/// <reference types="@types/jest" />
